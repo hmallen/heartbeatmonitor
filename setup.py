777b110445b565e78ad0e8083371fd242704d941
hmallen <allenhm@gmail.com>
2018-05-23 20:28:34 -0500

    Adding Python setup files for installation.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='HeartbeatMonitor',
+    version='0.1',
+    packages=['heartbeatmonitor'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
