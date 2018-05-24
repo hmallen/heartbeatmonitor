23da55b97b5595e04c80b0e9fc915e739a348bee
hmallen <allenhm@gmail.com>
2018-05-23 20:54:57 -0500

    Completed setup and build files. Testing successfully.

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='HeartbeatMonitor',
-    version='0.1',
+    name="HeartbeatMonitor",
+    version="0.1",
+    #packages=find_packages(),
+    #scripts=['heartbeatmonitor.py'],
     packages=['heartbeatmonitor'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    package_dir={'heartbeatmonitor': 'heartbeatmonitor'},
+
+    install_requires=['slackclient>=1.2.1'],
+
+    # metadata for upload to PyPI
+    author="Hunter M. Allen",
+    author_email="allenhm@gmail.com",
+    description="Central heartbeat monitoring application for multi-program deployment",
+    license="Creative Commons Attribution-Noncommercial-Share Alike license",
 )
