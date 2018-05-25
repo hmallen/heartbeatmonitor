418cabaf1a04bba82db4ce480d92cfe76e6603a2
hmallen <allenhm@gmail.com>
2018-05-24 20:59:51 -0500

    Repackaged installer.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Heartbeat Monitor
+
 Heartbeat monitor with integrated Slack alerts designed for use with multiple programs simultaneously.
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name='heartbeatmonitor',
-    version='0.1a',
+    version='0.1a1',
     author='Hunter M. Allen',
     author_email='allenhm@gmail.com',
     license='MIT',
@@ -10,6 +13,14 @@ setup(
     packages=['heartbeatmonitor'],
     #scripts=['bin/heartbeatmonitor.py'],
     install_requires=['slackclient>=1.2.1'],
-    description='Central heartbeat monitoring application for multi-program deployment',
+    description='Central heartbeat monitoring application for multi-program deployment.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hmallen/heartbeatmonitor',
     keywords=['heartbeat', 'monitor'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
 )
